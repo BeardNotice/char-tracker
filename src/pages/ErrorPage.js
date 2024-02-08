16cb43396b9cd668e121a4f03998fe2025335c3a
Ian Flurkey <ianflurkey@gmail.com>
2024-02-08 10:40:30 -0500

    added error page

---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,15 @@
+import { useRouteError } from "react-router-dom";
+
+function ErrorPage(){
+    const error = useRouteError();
+    console.error(error);
+
+    return(
+        <main>
+            <h1>404</h1>
+            <h2>Whoops! something went wrong!</h2>
+        </main>
+    )
+}
+
+export default ErrorPage;
