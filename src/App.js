242cf87ef032db6c3e01b8be96af4f1b646b3f0b
Ian Flurkey <ianflurkey@gmail.com>
2024-02-08 10:43:09 -0500

    updated app to show pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from "./components/NavBar";
+import {useState, useEffect} from "react";
+import {Outlet} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:4000/characters')
+     .then(res => res.json())
+     .then(data => setUsers(data))
+     .catch(err => console.error(err))
+     console.log(users)
+    }, [])
+
+    return (
+        <>
+        <header>
+            <NavBar />
+        </header>
+        <Outlet context={users} />
+        </>
+    );
+};
 
 export default App;
