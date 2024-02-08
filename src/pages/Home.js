86d9d4383632cfea4cbc138341c131e0724be099
Ian Flurkey <ianflurkey@gmail.com>
2024-02-08 10:41:08 -0500

    added home page

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+
+function Home(){
+    return(
+        <>
+        <main>
+            <h1>Candela Obscura Character Tracker</h1>
+            <p>Something eye-catching here!</p>
+        </main>
+        </>
+    );
+}
+export default Home
